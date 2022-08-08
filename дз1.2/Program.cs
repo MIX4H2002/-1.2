08efa849dddc0c0f39aebfde826d5e0de2de05d3
MIX4H <m-3k@mail.ru>
2022-08-08 22:06:42 +0300

    init proj

---
+++ дз1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿string name;
+
+Console.WriteLine("Как вас зовут?");
+name=Console.ReadLine();
+
+string familia;
+
+Console.WriteLine("Какая у вас фамилия?");
+familia=Console.ReadLine();
+
+
+
+string otchestvo;
+
+Console.WriteLine("Какое у вас отчество?");
+otchestvo=Console.ReadLine();
+
+
+string fullName= familia + " "+name+" "+otchestvo;
+Console.WriteLine(fullName);
